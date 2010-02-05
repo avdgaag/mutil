9328bdaba1218bc3bceaa453b916bc256d0c3fdd
Arjan van der Gaag <info@agwebdesign.nl>
2010-02-05 11:01:56 +0100

    Major bugfixes and minor refactoring after the first real kicking-the-tyres

--- mutil.js
+++ mutil.js
@@ -324,252 +324,254 @@ String.prototype.trim = function() {
  * @augments Enumerable
  * @borrows Enumerable#each as #each
  */
-function Mutil(arg, root) {
-	/**
-	 * Select DOM elements helper function.
-	 * 
-	 * @inner
-	 */
-	var find = function(arg, root) {
-		
-		// Helper method to convert a nodelist to an array
-		this.nodelistToArray = function(nodelist) {
-			var arr = [];
-			for(var i = 0, j = nodelist.length; i < j; i++) { arr.push(nodelist[i]); }
-			return arr;
+(function() {
+	function Mutil(arg, root) {
+		/**
+		 * Select DOM elements helper function.
+		 * 
+		 * @inner
+		 */
+		var find = function(arg, root) {
+
+			// Helper method to convert a nodelist to an array
+			this.nodelistToArray = function(nodelist) {
+				var arr = [];
+				for(var i = 0, j = nodelist.length; i < j; i++) { if(nodelist[i].nodeType == 1) arr.push(nodelist[i]); }
+				return arr;
+			};
+
+			// Set the root element to select from. Default to document.
+			// If there are multiple roots run this function that many times.
+			if(typeof root == 'undefined') root = document;
+			if(root == null) return [];
+			if(typeof root.length != 'undefined') {
+				return root.inject([], function(output) {
+					return output.concat($(arg, this));
+				});
+			}
+
+			// Select by NodeList: return a real array of elements
+			if(typeof arg.length != 'undefined' && typeof arg.item != 'undefined') return this.nodelistToArray(arg);
+
+			// Select by Node: return a node element
+			if(typeof arg.nodeType != 'undefined') return [arg];
+
+			// Select by ID: return the element
+			if(matches = arg.match(/^#([a-zA-Z0-9\-_]+)$/)) {
+				return [root.getElementById(matches[1])];
+			}
+
+			// Select multiple: find selectors seperated by commas
+			if(arg.match(/,/)) {
+				return arg.split(',').inject([], function(output) {
+					return output.concat($(this.trim()));
+				});
+			}
+
+			// Select by descendent: find elements inside other elements
+			// #foo bar table.test
+			if(arg.match(/ /)) {
+				var parts = arg.split(' '),
+				    parent = parts.shift(),
+				    selector = parts.join(' ');
+				return find(selector, find(parent, root));
+			}
+
+			// Select by classname: split by dot and find all elements, filter by the rest
+			if(arg.match(/[#a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_.]+/)) {
+				var parts = arg.split('.'),
+				    tagname = parts.shift(),
+				    classes = new RegExp(parts.join('|'), 'gi');
+				return find(tagname, root).select(function() {
+					return this.className.match(classes);
+				});
+			}
+
+			// Select by tagname: return array of elements
+			return this.nodelistToArray(root.getElementsByTagName(arg));
 		};
-		
-		// Set the root element to select from. Default to document.
-		// If there are multiple roots run this function that many times.
-		if(typeof root == 'undefined') root = document;
-		if(root == null) return [];
-		if(typeof root.length != 'undefined') {
-			return root.inject([], function(output) {
-				return output.concat($(arg, this));
-			});
-		}
 
-		// Select by NodeList: return a real array of elements
-		if(typeof arg.length != 'undefined' && typeof arg.item != 'undefined') return this.nodelistToArray(arg);
-		
-		// Select by Node: return a node element
-		if(typeof arg.nodeType != 'undefined') return [arg];
-
-		// Select by ID: return the element
-		if(matches = arg.match(/^#([a-zA-Z0-9\-_]+)$/)) {
-			return root.getElementById(matches[1]);
-		}
-
-		// Select multiple: find selectors seperated by commas
-		if(arg.match(/,/)) {
-			return arg.split(',').inject([], function(output) {
-				return output.concat($(this.trim()));
-			});
-		}
-
-		// Select by descendent: find elements inside other elements
-		// #foo bar table.test
-		if(arg.match(/ /)) {
-			var parts = arg.split(' '),
-			    parent = parts.shift(),
-			    selector = parts.join(' ');
-			return $(selector, $(parent, root));
-		}
-
-		// Select by classname: split by dot and find all elements, filter by the rest
-		if(arg.match(/[#a-zA-Z0-9\-_]+\.[a-zA-Z0-9\-_.]+/)) {
-			var parts = arg.split('.'),
-			    tagname = parts.shift(),
-			    classes = new RegExp(parts.join('|'), 'gi');
-			return $(tagname, root).select(function() {
-				return this.className.match(classes);
-			});
-		}
-
-		// Select by tagname: return array of elements
-		return this.nodelistToArray(root.getElementsByTagName(arg));
-	};
-	
-	/**
-	 * Register a load event function helper function.
-	 * 
-	 * @inner
-	 */
-	var addLoadEvent = function(func) {
-		var oldonload = window.onload;
-		if (typeof window.onload != 'function') {
-			window.onload = func;
-		} else {
-			window.onload = function() {
-				if (oldonload) oldonload();
-				func();
-			};
-		}
-	};
-	
-	if(typeof arguments[0] == 'function') {
-		addLoadEvent(arguments[0]);
-	} else {
-		this.elements = find(arguments[0], arguments[1]);
-	}
-}
-
-Mutil.prototype = {
-	/*
-	 * Import Enumerable functions
-	 * 
-	 * TODO: clean this up somehow.
-	 */
-	each:   function() { return Enumerable.prototype.each  .apply(this.elements, arguments); },
-	inject: function() { return Enumerable.prototype.inject.apply(this.elements, arguments); },
-	select: function() { return Enumerable.prototype.select.apply(this.elements, arguments); },
-	map:    function() { return Enumerable.prototype.map   .apply(this.elements, arguments); },
-	any:    function() { return Enumerable.prototype.any   .apply(this.elements, arguments); },
-	
-	/**
-	 * Add an event to a DOM element.
-	 * 
-	 * Usage:
-	 * 
-	 * <pre class="code">
-	 * addEvent(document.getElementById('test'), 'click', function(e) {
-	 *   console.log('clicked!');
-	 * });
-	 * </pre>
-	 * 
-	 * You can also use this method as an array function, applying it to
-	 * all elements in an array.
-	 * 
-	 * The single argument to the callback function is an {@link Event} object
-	 * that works just like the native event object, but is cross-browser
-	 * compatible.
-	 * 
-	 * @see Event
-	 * @param {String} type is the event to observe.
-	 * @param {Function} callback is the observer function.
-	 * @returns itself for chaining purposes.
-	 * @type Mutil
-	 */
-	addEvent: function(type, callback) {
-		function FwEvent(e) {
-			var base = this;
-			this.event = e || window.event;
-			this.target = this.event.target || this.event.srcElement;
-			this.stopPropagation = function() {
-				e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true;
-			};
-			this.preventDefault = function() {
-				e.preventDefault ? e.preventDefault() : e.returnValue = false;
-			};
-			this.stop = function() {
-				this.stopPropagation();
-				this.preventDefault();
-			};
-		}
-		var add = function(el, fn) {
-			if(window.addEventListener) {
-				el.addEventListener(type, fn, false);
-			} else if(window.attachEvent) {
-				el.attachEvent('on' + type, fn);
+		/**
+		 * Register a load event function helper function.
+		 * 
+		 * @inner
+		 */
+		var addLoadEvent = function(func) {
+			var oldonload = window.onload;
+			if (typeof window.onload != 'function') {
+				window.onload = func;
 			} else {
-				el['on' + type] = fn;
+				window.onload = function() {
+					if (oldonload) oldonload();
+					func();
+				};
 			}
 		};
-		this.each(function() {
-			add(this, function(e) { callback.call(this, new FwEvent(e)); });
-		});
-		return this;
-	},
-	
-	/**
-	 * Give an element a class.
-	 * 
-	 * @example
-	 * $('p').addClass('highlight');
-	 * 
-	 * @param {String} value is the class name to add
-	 * @returns itself for chaining purposes
-	 * @type Mutil
-	 */
-	addClass: function(value) {
-		return this.each(function() {
-			if(!this.className)
-				this.className = value;
-			else if(!this.className.match(new RegExp('(\\s|^)' + value + '(\\s|$)')))
-				this.className += ' ' + value;
-		});
-	},
-	
-	/**
-	 * Remove an element's class.
-	 * 
-	 * @example
-	 * $('p').removeClass('highlight');
-	 * 
-	 * @param {String} value is the class name to remove
-	 * @returns itself for chaining purposes
-	 * @type Mutil
-	 */
-	removeClass: function(value) {
-		return this.each(function() {
-			this.className = this.className.replace(new RegExp('(\\s|^)' + value + '(\\s|$)'), '');
-		});
-	},
-	
-	/**
-	 * Tell if an element has a given class.
-	 * 
-	 * When working on multiple elements this returns whether ALL the elements
-	 * have the given class.
-	 * 
-	 * @example
-	 * if($('p').hasClass('highlight')) { ... }
-	 * 
-	 * @param {String} value is the class name to add
-	 * @returns whether the element(s) has the class or not
-	 * @type Boolean
-	 */
-	hasClass: function(value) {
-		return this.inject(false, function(m) {
-			return m || this.className.match(new RegExp('(\\s|^)' + value + '(\\s|$)'));
-		});
-	},
-	
-	/**
-	 * Switch an element's class on or off.
-	 * 
-	 * Either remove or add the class to the element, depending on
-	 * whether the element already has the class.
-	 * 
-	 * @param {String} value the class name to switch on or off
-	 * @returns itself for chaining purposes
-	 * @type Mutil
-	 */
-	toggleClass: function(value) {
-		return this.each(function() {
-			$(this).hasClass(value) ? $(this).removeClass(value) : $(this).addClass(value);
-		});
-	},
-	
-	/**
-	 * Give an element an arbitrary set of CSS properties.
-	 * 
-	 * @example
-	 * $('p').setCss({ color: '#f00' });
-	 * 
-	 * @param {Object} key/value object of CSS attributes and values.
-	 * @returns itself for chaining purposes
-	 * @type Mutil
-	 */
-	setCss: function(styles) {
-		return this.each(function() {
-			for(var prop in styles) {
-				if(!styles.hasOwnProperty(prop)) continue;
-				this.style[prop] = styles[prop];
-			}
-		});
-	}
-};
 
-window.mutil = window.$ = function(arg, root) { return new Mutil(arg, root); };
-window.mutil.fn = Mutil.prototype;
+		if(typeof arguments[0] == 'function' && (!Function.prototype.call || typeof(arguments[0].call) == 'function')) {
+			addLoadEvent(arguments[0]);
+		} else {
+			this.elements = find(arguments[0], arguments[1]);
+		}
+	}
+
+	Mutil.prototype = {
+		/*
+		 * Import Enumerable functions
+		 * 
+		 * TODO: clean this up somehow.
+		 */
+		each:   function() { return Enumerable.prototype.each  .apply(this.elements, arguments); },
+		inject: function() { return Enumerable.prototype.inject.apply(this.elements, arguments); },
+		select: function() { return Enumerable.prototype.select.apply(this.elements, arguments); },
+		map:    function() { return Enumerable.prototype.map   .apply(this.elements, arguments); },
+		any:    function() { return Enumerable.prototype.any   .apply(this.elements, arguments); },
+
+		/**
+		 * Add an event to a DOM element.
+		 * 
+		 * Usage:
+		 * 
+		 * <pre class="code">
+		 * addEvent(document.getElementById('test'), 'click', function(e) {
+		 *   console.log('clicked!');
+		 * });
+		 * </pre>
+		 * 
+		 * You can also use this method as an array function, applying it to
+		 * all elements in an array.
+		 * 
+		 * The single argument to the callback function is an {@link Event} object
+		 * that works just like the native event object, but is cross-browser
+		 * compatible.
+		 * 
+		 * @see Event
+		 * @param {String} type is the event to observe.
+		 * @param {Function} callback is the observer function.
+		 * @returns itself for chaining purposes.
+		 * @type Mutil
+		 */
+		addEvent: function(type, callback) {
+			function FwEvent(e) {
+				var base = this;
+				this.event = e || window.event;
+				this.target = this.event.target || this.event.srcElement;
+				this.stopPropagation = function() {
+					e.stopPropagation ? e.stopPropagation() : e.cancelBubble = true;
+				};
+				this.preventDefault = function() {
+					e.preventDefault ? e.preventDefault() : e.returnValue = false;
+				};
+				this.stop = function() {
+					this.stopPropagation();
+					this.preventDefault();
+				};
+			}
+			var add = function(el, fn) {
+				if(window.addEventListener) {
+					el.addEventListener(type, fn, false);
+				} else if(window.attachEvent) {
+					el.attachEvent('on' + type, fn);
+				} else {
+					el['on' + type] = fn;
+				}
+			};
+			this.each(function() {
+				add(this, function(e) { callback.call(this, new FwEvent(e)); });
+			});
+			return this;
+		},
+
+		/**
+		 * Give an element a class.
+		 * 
+		 * @example
+		 * $('p').addClass('highlight');
+		 * 
+		 * @param {String} value is the class name to add
+		 * @returns itself for chaining purposes
+		 * @type Mutil
+		 */
+		addClass: function(value) {
+			return this.each(function() {
+				if(!this.className)
+					this.className = value;
+				else if(!this.className.match(new RegExp('(\\s|^)' + value + '(\\s|$)')))
+					this.className += ' ' + value;
+			});
+		},
+
+		/**
+		 * Remove an element's class.
+		 * 
+		 * @example
+		 * $('p').removeClass('highlight');
+		 * 
+		 * @param {String} value is the class name to remove
+		 * @returns itself for chaining purposes
+		 * @type Mutil
+		 */
+		removeClass: function(value) {
+			return this.each(function() {
+				this.className = this.className.replace(new RegExp('(\\s|^)' + value + '(\\s|$)'), '');
+			});
+		},
+
+		/**
+		 * Tell if an element has a given class.
+		 * 
+		 * When working on multiple elements this returns whether ALL the elements
+		 * have the given class.
+		 * 
+		 * @example
+		 * if($('p').hasClass('highlight')) { ... }
+		 * 
+		 * @param {String} value is the class name to add
+		 * @returns whether the element(s) has the class or not
+		 * @type Boolean
+		 */
+		hasClass: function(value) {
+			return this.inject(false, function(m) {
+				return m || this.className.match(new RegExp('(\\s|^)' + value + '(\\s|$)'));
+			});
+		},
+
+		/**
+		 * Switch an element's class on or off.
+		 * 
+		 * Either remove or add the class to the element, depending on
+		 * whether the element already has the class.
+		 * 
+		 * @param {String} value the class name to switch on or off
+		 * @returns itself for chaining purposes
+		 * @type Mutil
+		 */
+		toggleClass: function(value) {
+			return this.each(function() {
+				$(this).hasClass(value) ? $(this).removeClass(value) : $(this).addClass(value);
+			});
+		},
+
+		/**
+		 * Give an element an arbitrary set of CSS properties.
+		 * 
+		 * @example
+		 * $('p').setCss({ color: '#f00' });
+		 * 
+		 * @param {Object} key/value object of CSS attributes and values.
+		 * @returns itself for chaining purposes
+		 * @type Mutil
+		 */
+		setCss: function(styles) {
+			return this.each(function() {
+				for(var prop in styles) {
+					if(!styles.hasOwnProperty(prop)) continue;
+					this.style[prop] = styles[prop];
+				}
+			});
+		}
+	};
+
+	window.mutil = window.$ = function(arg, root) { return new Mutil(arg, root); };
+	window.mutil.fn = Mutil.prototype;
+})();
